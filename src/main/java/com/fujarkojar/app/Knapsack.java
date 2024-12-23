c9184eedc15b090bd33bf41ad9d157bdef77284c
Daisuke Takahashi <daisuke332003@gmail.com>
2024-12-23 15:30:30 -0800

    Update Knapsack.

--- src/main/java/com/fujarkojar/app/Knapsack.java
+++ src/main/java/com/fujarkojar/app/Knapsack.java
@@ -9,22 +9,26 @@ public class Knapsack {
         for (int[] a : dp) {
             Arrays.fill(a, Integer.MIN_VALUE);
         }
-        return helperByMemoization(weight, value, n, maxWeight, 0, 0, dp);
+        return helperByMemoization(weight, value, n, maxWeight, n - 1, dp);
     }
 
-    private static int helperByMemoization(int[] weight, int[] value, int n, int maxWeight, int ind, int curWeight, int[][] dp) {
-        if (ind > n - 1) { // Base case
-            return 0;
+    private static int helperByMemoization(int[] weight, int[] value, int n, int maxWeight, int ind, int[][] dp) {
+        if (ind == 0) { // Base case
+            if (weight[0] <= maxWeight) {
+                return value[0];
+            } else {
+                return 0;
+            }
         }
         // Memoization
-        if (dp[ind][curWeight] != Integer.MIN_VALUE) {
-            return dp[ind][curWeight];
+        if (dp[ind][maxWeight] != Integer.MIN_VALUE) {
+            return dp[ind][maxWeight];
         }
-        int curValue = helperByMemoization(weight, value, n, maxWeight, ind + 1, curWeight, dp); // Not take
-        if (curWeight + weight[ind] <= maxWeight) { // Take
-            curValue = Math.max(curValue, value[ind] + helperByMemoization(weight, value, n, maxWeight, ind + 1, curWeight + weight[ind], dp));
+        int curValue = helperByMemoization(weight, value, n, maxWeight, ind - 1, dp); // Not take
+        if (weight[ind] <= maxWeight) { // Take
+            curValue = Math.max(curValue, value[ind] + helperByMemoization(weight, value, n, maxWeight - weight[ind], ind - 1, dp));
         }
-        dp[ind][curWeight] = curValue;
+        dp[ind][maxWeight] = curValue;
         return curValue;
     }
 
