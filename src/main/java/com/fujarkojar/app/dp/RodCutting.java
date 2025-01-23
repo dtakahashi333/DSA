e0c0ac288cbabec713ee645958f616f689f44faf
Daisuke Takahashi <daisuke332003@gmail.com>
2025-01-22 18:43:29 -0800

    Add alternative solutions for RodCutting.

--- src/main/java/com/fujarkojar/app/dp/RodCutting.java
+++ src/main/java/com/fujarkojar/app/dp/RodCutting.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 // Rod cutting problem
 // https://www.naukri.com/code360/problems/rod-cutting-problem_800284?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+// https://www.youtube.com/watch?v=mO8XpGoJwuo&t=757s
 public class RodCutting {
 
     public static int solveByMemoization(int[] price, int n) {
@@ -44,5 +45,57 @@ public class RodCutting {
             }
         }
         return dp[n];
+    }
+
+    public static int solveByMemoization2(int[] price, int n) {
+        int[][] dp = new int[n][n + 1];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        return helperByMemoization2(price, n - 1, n, dp);
+    }
+
+    private static int helperByMemoization2(int[] price, int ind, int n, int[][] dp) {
+        // Base cases
+        if (ind == 0) {
+            return price[ind] * n;
+        }
+        // Memoization
+        if (dp[ind][n] != -1) {
+            return dp[ind][n];
+        }
+        // Not take
+        int notTake = helperByMemoization2(price, ind - 1, n, dp);
+        // Take
+        int take = Integer.MIN_VALUE;
+        if (ind + 1 <= n) {
+            take = price[ind] + helperByMemoization2(price, ind, n - ind - 1, dp);
+        }
+        dp[ind][n] = Math.max(notTake, take);
+        return dp[ind][n];
+    }
+
+    public static int solveByTabulation2(int[] price, int n) {
+        int[][] dp = new int[n][n + 1];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        // Tabulation
+        for (int k = 0; k <= n; ++k) {
+            dp[0][k] = k * price[0];
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int k = 0; k <= n; ++k) {
+                // Not take
+                int notTake = dp[i - 1][k];
+                // Take
+                int take = Integer.MIN_VALUE;
+                if (i + 1 <= k) {
+                    take = price[i] + dp[i][k - i - 1];
+                }
+                dp[i][k] = Math.max(notTake, take);
+            }
+        }
+        return dp[n - 1][n];
     }
 }
--- src/test/java/com/fujarkojar/app/dp/RodCuttingTest.java
+++ src/test/java/com/fujarkojar/app/dp/RodCuttingTest.java
@@ -42,4 +42,36 @@ class RodCuttingTest {
         int expected = 24;
         assertEquals(expected, RodCutting.solveByTabulation(price, n));
     }
+
+    @Test
+    void solveByMemoization2_1() {
+        int[] price = {2, 5, 7, 8, 10};
+        int n = 5;
+        int expected = 12;
+        assertEquals(expected, RodCutting.solveByMemoization2(price, n));
+    }
+
+    @Test
+    void solveByMemoization2_2() {
+        int[] price = {3, 5, 8, 9, 10, 17, 17, 20};
+        int n = 8;
+        int expected = 24;
+        assertEquals(expected, RodCutting.solveByMemoization2(price, n));
+    }
+
+    @Test
+    void solveByTabulation2_1() {
+        int[] price = {2, 5, 7, 8, 10};
+        int n = 5;
+        int expected = 12;
+        assertEquals(expected, RodCutting.solveByTabulation2(price, n));
+    }
+
+    @Test
+    void solveByTabulation2_2() {
+        int[] price = {3, 5, 8, 9, 10, 17, 17, 20};
+        int n = 8;
+        int expected = 24;
+        assertEquals(expected, RodCutting.solveByTabulation2(price, n));
+    }
 }
