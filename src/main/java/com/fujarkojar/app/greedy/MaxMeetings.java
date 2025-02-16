6713e5029c315ef4c83ce586fbcd7704873c6aba
Daisuke Takahashi <daisuke332003@gmail.com>
2025-02-16 13:52:49 -0800

    Add a dynamic programming solution to MaxMeetings.

--- src/main/java/com/fujarkojar/app/greedy/MaxMeetings.java
+++ src/main/java/com/fujarkojar/app/greedy/MaxMeetings.java
@@ -19,7 +19,7 @@ public class MaxMeetings implements Comparator<Integer[]> {
         }
     }
 
-    public static int solve(int[] start, int[] end) {
+    public static int solveByGreedy(int[] start, int[] end) {
         if (start.length != end.length) {
             return -1;
         }
@@ -43,5 +43,47 @@ public class MaxMeetings implements Comparator<Integer[]> {
             }
         }
         return count;
+    }
+
+    public static int solveByMemoization(int[] start, int[] end) {
+        // Add an extra items.
+        int[] startEx = new int[start.length + 2];
+        int[] endEx = new int[end.length + 2];
+        int n = startEx.length;
+        startEx[0] = endEx[0] = -1;
+        startEx[n - 1] = endEx[n - 1] = Integer.MAX_VALUE;
+        for (int i = 1; i < n - 1; ++i) {
+            startEx[i] = start[i - 1];
+            endEx[i] = end[i - 1];
+        }
+        int[][] dp = new int[n][n];
+        for (int[] a : dp) {
+            Arrays.fill(a, -1);
+        }
+        return helperByMemoization(startEx, endEx, 0, n - 1, dp);
+    }
+
+    private static int helperByMemoization(int[] start, int[] end, int i, int j, int[][] dp) {
+        // Base case
+        if (i >= j) {
+            return 0;
+        }
+        int n = start.length;
+        // Memoization
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int maxCount = 0;
+        for (int k = 1; k < n - 1; ++k) {
+            if (start[k] > end[i] && end[k] < start[j]) {
+                maxCount = Math.max(
+                        maxCount,
+                        helperByMemoization(start, end, i, k, dp) // first half
+                                + helperByMemoization(start, end, k, j, dp) // second half
+                                + 1);
+            }
+        }
+        dp[i][j] = maxCount;
+        return maxCount;
     }
 }
--- src/test/java/com/fujarkojar/app/greedy/MaxMeetingsTest.java
+++ src/test/java/com/fujarkojar/app/greedy/MaxMeetingsTest.java
@@ -12,26 +12,50 @@ class MaxMeetingsTest {
     }
 
     @Test
-    void solve1() {
+    void solveByGreedy1() {
         int[] start = {1, 3, 0, 5, 8, 5};
         int[] end = {2, 4, 6, 7, 9, 9};
         int expected = 4;
-        assertEquals(expected, MaxMeetings.solve(start, end));
+        assertEquals(expected, MaxMeetings.solveByGreedy(start, end));
     }
 
     @Test
-    void solve2() {
+    void solveByGreedy2() {
         int[] start = {10, 12, 20};
         int[] end = {20, 25, 30};
         int expected = 1;
-        assertEquals(expected, MaxMeetings.solve(start, end));
+        assertEquals(expected, MaxMeetings.solveByGreedy(start, end));
     }
 
     @Test
-    void solve3() {
+    void solveByGreedy3() {
         int[] start = {1, 2};
         int[] end = {100, 99};
         int expected = 1;
-        assertEquals(expected, MaxMeetings.solve(start, end));
+        assertEquals(expected, MaxMeetings.solveByGreedy(start, end));
+    }
+
+    @Test
+    void solveByMemoization1() {
+        int[] start = {1, 3, 0, 5, 8, 5};
+        int[] end = {2, 4, 6, 7, 9, 9};
+        int expected = 4;
+        assertEquals(expected, MaxMeetings.solveByMemoization(start, end));
+    }
+
+    @Test
+    void solveByMemoization2() {
+        int[] start = {10, 12, 20};
+        int[] end = {20, 25, 30};
+        int expected = 1;
+        assertEquals(expected, MaxMeetings.solveByMemoization(start, end));
+    }
+
+    @Test
+    void solveByMemoization3() {
+        int[] start = {1, 2};
+        int[] end = {100, 99};
+        int expected = 1;
+        assertEquals(expected, MaxMeetings.solveByMemoization(start, end));
     }
 }
